db5f9d6ec846ea5c4185a7f18d9776526c1cad9b
alexandrbarulin <alexandrbarulin2003@gmail.com>
2021-11-01 14:58:28 +0200

    Lab_3 Pattern

---
+++ Lab_3 Pattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        public string Date { get; private set; }
+        public static string text = "hello";
+        private Singleton()
+        {
+            Console.WriteLine($"Singleton ctor {DateTime.Now}");
+            Date = DateTime.Now.ToString();
+        }
+
+        public static Singleton GetInstance()
+        {
+            Console.WriteLine($"GetInstance {DateTime.Now}");
+            return Nested.instance;
+        }
+
+        private class Nested
+        {
+            internal static readonly Singleton instance = new Singleton();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main {DateTime.Now}");
+            Console.WriteLine(Singleton.text);
+            Console.Read();
+        }
+    }
+}
